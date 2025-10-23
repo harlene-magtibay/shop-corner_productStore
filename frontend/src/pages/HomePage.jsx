7612b591f7aef603c4752a91d838af906e3b046d
harlene-magtibay <magtibayharlene08@gmail.com>
2025-10-24 04:10:26 +0800

    Create the home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { Container, SimpleGrid, Text, VStack } from '@chakra-ui/react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useProductStore } from '../store/product'
+import ProductCard from '../components/ProductCard'
 
 const HomePage = () => {
+  const {fetchProducts, products} = useProductStore();
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
   return (
-    <div>HomePage</div>
-  )
+    <Container maxW="Container.xl" py={12} px={'15%'}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, #9b255eff, #d84fbdff)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Available Products🏷️
+        </Text>
+
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+            lg: 3,
+          }}
+          spacing={10}
+          w={"full"}
+        >
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </SimpleGrid>
+
+        {products.length === 0 && (
+          <Text
+            fontSize="xl"
+            textAlign={"center"}
+            fontWeight="bold"
+            color="gray.500"
+          >
+            No products found.
+            <Link to={"/create"}>
+              <Text
+                color="blue.400"
+                _hover={{ textDecoration: "underline" }}
+                fontWeight={"bold"}
+              >
+                Create a product
+              </Text>
+            </Link>
+          </Text>
+        )}
+      </VStack>
+    </Container>
+  );
 }
 
 export default HomePage
